795b9943163ebfd64b71220f3d28054af60f127f
Gloria Budiman <wahyu.g@gmail.com>
2020-06-25 03:04:51 -0700

    Remote heroku

--- src/utils/remote.ts
+++ src/utils/remote.ts
@@ -5,7 +5,7 @@ interface IRemote {
 
 export default function remote(): IRemote {
   const isLocal = Boolean(window.location.host.match(/localhost/));
-  const rootUrl = '//localhost:3000';
+  const rootUrl = isLocal ? '//localhost:3000' : '//grayce-be.herokuapp.com';
   const apiUrl = `${rootUrl}`;
 
   return {
